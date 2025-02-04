624fd4952099b20ed15c08b2070ae48557187e87
dongyun <0902hdy@gmail.com>
2025-02-04 14:45:16 +0900

    initial commit

---
+++ src/main/java/com/example/learningsquad/LearningsquadApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningsquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningsquadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningsquadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learningsquad/LearningsquadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningsquad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningsquadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
